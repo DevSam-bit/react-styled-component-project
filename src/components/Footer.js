c571deacf12156d08971b9de04a5e2113e1d8141
devsam-bit <devsam-bit@outlook.com>
2021-10-11 00:36:14 +0300

    styled-component

---
+++ src/components/Footer.js
@@ -0,0 +1,36 @@
+import { Container } from './styles/Container.styled'
+import { Flex } from './styles/Flex.styled'
+import { StyledFooter } from './styles/Footer.styled'
+import SocialIcons from './SocialIcons'
+
+export default function Footer() {
+    return (
+        <StyledFooter>
+            <Container>
+                <img src="./images/logo_white.svg" alt="" />
+
+                <Flex>
+                    <ul>
+                        <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum illo ipsum molestiae laboriosam est voluptatibus.</li>
+                        <li>+234567890</li>
+                        <li>example@gmail.com</li>
+                    </ul>
+                    <ul>
+                        <li>About Us</li>
+                        <li>What We Do</li>
+                        <li>FAQ</li>
+                    </ul>
+                    <ul>
+                        <li>Career</li>
+                        <li>Blog</li>
+                        <li>Contact Us</li>
+                    </ul>
+
+                    <SocialIcons />
+                </Flex>
+
+                <p>&copy; 2021 Huddle. All rights reserved</p>
+            </Container>
+        </StyledFooter>
+    )
+}
